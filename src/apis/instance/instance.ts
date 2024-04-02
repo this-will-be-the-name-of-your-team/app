3ee377709b42ae66b79249e5faa42fb954919956
유성욱 <seonguk@yuseong-ug-ui-noteubug.local>
2024-04-02 11:28:13 +0900

    add intercepter

---
+++ src/apis/instance/instance.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: "",
+  headers: { "Content-Type": "application/json" },
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+
+    return config;
+  },
+  (error) => {
+    return error;
+  },
+);
+
+instance.interceptors.response.use(
+  async (res) => {
+    return res;
+  },
+  async (error) => {
+    // error logic
+    console.log(error);    
+    return Promise.reject(error);
+  },
+);
