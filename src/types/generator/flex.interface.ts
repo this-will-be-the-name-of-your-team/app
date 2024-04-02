5ee5f64655708ee6b3b27870cf3f2cd76c05ed47
원설아 <seolalny@gmail.com>
2024-04-02 17:27:43 +0900

    add: flexGenerator 추가

---
+++ src/types/generator/flex.interface.ts
@@ -0,0 +1,5 @@
+export interface FlexProps {
+  direction?: React.CSSProperties["flexDirection"];
+  justify?: React.CSSProperties["justifyContent"];
+  align?: React.CSSProperties["alignItems"];
+}
