98af6c31ccfb0ba23cf8155ea0a6908dadd76e00
seola1ne <seolalny@gmail.com>
2024-05-09 13:45:49 +0900

    chore : primary 색상 추가

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,4 +1,13 @@
 export const theme = {
   white: "#FFFFFF",
   black: "#000000",
+  primary100: "#FFDED0",
+  primary200: "#FFBCA2",
+  primary300: "#FF9B73",
+  primary400: "#FF7945",
+  primary500: "#FF5816",
+  primary600: "#CC4612",
+  primary700: "#99350D",
+  primary800: "#662309",
+  primary900: "#331204",
 };
