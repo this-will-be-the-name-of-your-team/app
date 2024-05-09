2b62117e51ba83981983b7d9d8f870776a533d34
Ubinquitous <ubinquitous1@gmail.com>
2024-05-09 13:38:22 +0900

    chore : 임시 image upload

---
+++ src/app/api/image/route.ts
@@ -0,0 +1,20 @@
+import { writeFile } from "fs/promises";
+import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+
+export async function POST(req: NextRequest) {
+  const formData = await req.formData();
+  const file = formData.get("file") as File;
+  if (!file) return NextResponse.json({ status: 400, data: "No files received" });
+
+  const buffer = Buffer.from(await file.arrayBuffer());
+  const filename = `${Date.now()}_${file.name}`;
+  try {
+    const pathname = path.join(process.cwd(), "public/database/" + filename);
+    const url = path.join("/public/database/" + filename);
+    await writeFile(pathname, buffer);
+    return NextResponse.json({ status: 201, url });
+  } catch (error) {
+    return NextResponse.json({ status: 500, data: error });
+  }
+}
