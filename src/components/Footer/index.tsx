5c8ec82ee0dfe511625d467c48174473a4151520
baekseojin <seojinbaek100@gmail.com>
2024-05-30 11:50:10 +0900

    feat: footer 추가

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,19 @@ import { styled } from "styled-components";
 const Footer = () => {
   return (
     <FooterContainer>
-      <Title>MINGLE</Title>
+      <TextContainer>
+        <Title>MINGLE MOOD</Title>
+        <Text>
+          상호명 : 밍글 | 대표 : 황다교
+          <br />
+          ADD. 부산광역시 어쩌고 저쩌고 123-456번지 주소
+          <br />
+          T. 010-1234-5678 | F.010-1234-5678 | E. ming9ris@naver.com
+        </Text>
+      </TextContainer>
+      <IconContainer>
+        <Copyright>COPYRIGHT © 2024 MINGLE MOOD Co., Ltd. ALL RIGHTS RESERVED.</Copyright>
+      </IconContainer>
     </FooterContainer>
   );
 };
@@ -15,14 +27,34 @@ const FooterContainer = styled.footer`
   bottom: 0rem;
   position: fixed;
   display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 200px;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 100px;
 `;
 
 const Title = styled.div`
-  color: white;
+  color: #ff5816;
   font-size: 40px;
   font-weight: bold;
-  margin-left: 11.25rem;
-  margin-top: 1.25rem;
+`;
+
+const Text = styled.div`
+  color: #999999;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-direction; column;
+`;
+
+const Copyright = styled.div`
+  color: #999999;
 `;
 
 export default Footer;
