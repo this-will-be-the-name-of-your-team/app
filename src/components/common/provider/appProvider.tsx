af51587a9240b6cbb7a445bf30e92b9950feb8d4
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-05-30 11:10:16 +0900

    add: provider 에 헤더 추가

--- src/components/common/provider/appProvider.tsx
+++ src/components/common/provider/appProvider.tsx
@@ -3,11 +3,13 @@
 import { ReactNode } from "react";
 import { GlobalStyle } from "@/styles/global";
 import StyledComponentsRegistry from "@/app/lib/regisrty";
+import Header from "@/components/Header";
 
 const AppProvider = ({ children }: { children: ReactNode }) => {
   return (
     <StyledComponentsRegistry>
       <GlobalStyle />
+      <Header />
       {children}
     </StyledComponentsRegistry>
   );
